1fde1bf9a1abe7d0c50a485ce29dbb4e253a8eae
lanayepifanova <yepifanova.lana@gmail.com>
2025-04-27 13:41:00 -0400

    slider spacing

--- src/App.tsx
+++ src/App.tsx
@@ -258,11 +258,11 @@ export default function App() {
               <div className="text-[9px] font-light uppercase tracking-[0.3em] text-white/60">
                 Session
               </div>
-              <div className="mt-8 h-4" />
+              <div className="mt-12 h-8" />
               <div className="text-[9px] font-light uppercase tracking-[0.3em] text-white/55">
                 Speed
               </div>
-              <div className="mt-4 flex flex-col gap-2">
+              <div className="mt-8 flex flex-col gap-6">
                 <Slider
                   value={[shaderSpeed]}
                   min={0.1}
@@ -270,7 +270,7 @@ export default function App() {
                   step={0.05}
                   orientation="horizontal"
                   onValueChange={(value) => setShaderSpeed(value[0])}
-                  className="w-44 [&_[data-slot=slider-track]]:h-1.5 [&_[data-slot=slider-track]]:bg-white/10 [&_[data-slot=slider-range]]:bg-white/70 [&_[data-slot=slider-thumb]]:size-3.5 [&_[data-slot=slider-thumb]]:border-white/60 [&_[data-slot=slider-thumb]]:bg-white"
+                  className="w-52 [&_[data-slot=slider-track]]:h-2 [&_[data-slot=slider-track]]:bg-white/25 [&_[data-slot=slider-range]]:bg-white/80 [&_[data-slot=slider-thumb]]:size-3.5 [&_[data-slot=slider-thumb]]:border-white/70 [&_[data-slot=slider-thumb]]:bg-white"
                   aria-label="Animation speed"
                 />
                 <div className="text-right text-[9px] font-light uppercase tracking-[0.24em] text-white/50">
